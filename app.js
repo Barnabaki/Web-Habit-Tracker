d8b86a44d9b2f04c90799ba72e9e496c4e2c1266
Barnaba Kiciński <117103636+Barnabaki@users.noreply.github.com>
2023-10-30 17:27:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const calendarContainer = document.getElementById('calendar');
 const weekDays = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
 
 function days_in_month(year, month) {
+  console.log('days_in_month')
   return new Date(year, month + 1, 0).getDate();
 }
 
 function is_leap_year(year) {
+  console.log('is_leap_year')
   return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
 }
 
 function formatDate(date) {
+  console.log('formatDate')
   const year = date.getFullYear().toString();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
@@ -17,13 +20,15 @@ function formatDate(date) {
 }
 
 function createSquare(id) {
+  console.log('createSquare')
   const square = document.createElement('div');
-  square.className = 'square noActivity';
+  square.className = 'noActivity';
   square.id = formatDate(id);
   return square;
 }
 
 function getFirstMondayDate(year) {
+  console.log('getFirstMondayDate')
   const date = new Date(year, 0, 1);
   const dayOfWeek = date.getDay();
 
@@ -37,6 +42,7 @@ function getFirstMondayDate(year) {
 
   return date;
 }
+
 
 const currentYear = new Date().getFullYear();
 const initialDate = getFirstMondayDate(currentYear);
@@ -74,18 +80,16 @@ for (let week = 0; week < 54; week++) {
         const square = createSquare(new Date(date));
         col.appendChild(square);
         date.setDate(date.getDate() + 1);
-      } else {
-        const square = createSquare('');
-        col.appendChild(square);
       }
     }
   }
 }
 
 
-const nextButton = document.getElementById('nextButton');
+const nextButton = document.querySelector('#nextButton');
 
-nextButton.addEventListener('click', () => {
+nextButton.onclick = function () {
+  console.log('nextButton');
   // Get the current date
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
@@ -93,39 +97,30 @@ nextButton.addEventListener('click', () => {
   const currentDay = currentDate.getDate();
   const formattedDate = `${currentDay}.${currentMonth}.${currentYear}`;
 
-  // Find the square with the current date's ID
-  const square = document.getElementById(formattedDate);
+  // Find the square with the current date's ID and pass the ID to addClassToElement
+  addClassToElement(formattedDate);
+};
 
-  // Check if the square exists
-  if (square = ".square") {
-    // Change the class of the square
-    square.classList.add('colored');
+function addClassToElement(formattedDate) {
+  var element = document.getElementById(formattedDate);
+
+  if (element) {
+    console.log('addClassToElement');
+    var classes = ['activityLevel-1', 'activityLevel-2', 'activityLevel-3', 'activityLevel-4', 'noActivity'];
+
+    // Get the current class name of the element
+    var currentClass = element.className;
+
+    // Remove any existing classes from the element
+    element.className = '';
+
+    // Find the index of the current class in the array
+    var currentIndex = classes.indexOf(currentClass);
+
+    // Calculate the index of the next class
+    var nextIndex = (currentIndex + 1) % classes.length;
+
+    // Add the next class to the element
+    element.classList.add(classes[nextIndex]);
   }
-});
-
-document.getElementById('colourButton').addEventListener('click', function () {
-    addClassToElement('02.01.2023');
-});
-
-function addClassToElement(elementId) {
-    var element = document.getElementById(elementId);
-
-    if (element) {
-        var classes = ['colored', 'square'];
-
-        // Get the current class name of the element
-        var currentClass = element.className;
-
-        // Remove any existing classes from the element
-        element.className = '';
-
-        // Find the index of the current class in the array
-        var currentIndex = classes.indexOf(currentClass);
-
-        // Calculate the index of the next class
-        var nextIndex = (currentIndex + 1) % classes.length;
-
-        // Add the next class to the element
-        element.classList.add(classes[nextIndex]);
-    }
 }
